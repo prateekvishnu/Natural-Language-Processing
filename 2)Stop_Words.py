729d327e5070dd10f71a531214e0469002545377
Vishnu Prateek K <prateekvishnu@gmail.com>
2016-07-30 21:23:59 +0530

    Stop Words Removal

---
+++ 2)Stop_Words.py
@@ -0,0 +1,17 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+example_sentence = "This is an example showing how to remove stop words from a sentence."
+
+#nltk package contains a stop words corpus that can be imported
+
+stop_words = set(stopwords.words("english"))
+
+words = word_tokenize(example_sentence)
+
+
+#removing stop words from the sentence.
+
+filtered_sentence = [w for w in words if not w in stop_words]
+
+print(filtered_sentence)
